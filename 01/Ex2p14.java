2c915f1b9c7ead8de0a5cecc685087aee7d8ada5
Heinriches <heydrich.institut@gmail.com>
2026-01-30 14:08:23 -0300

    Update Ex2p14.java

--- 01/Ex2p14.java
+++ 01/Ex2p14.java
@@ -8,6 +8,12 @@
 
 public class Ex2p14{
     public static void main(String[] args){
+
         System.out.print("1 2 3 4");
+
+        System.out.println("1 2 3 4");
+        System.out.println("1 2 3 4");
+        System.out.println("1 2 3 4");
+        System.out.println("1 2 3 4");
     }
 }
