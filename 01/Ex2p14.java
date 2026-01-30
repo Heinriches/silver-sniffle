d2870ffd69c02cdcc86deace5fd170c91f4ea51e
Heinriches <heydrich.institut@gmail.com>
2026-01-30 13:58:23 -0300

    Update Ex2p14.java

---
+++ 01/Ex2p14.java
@@ -0,0 +1,13 @@
+//  Exercicio 2.14:
+//                  Escreva um aplicativo que exiba os números de 1 a 4 na mesma linha, com
+//                  cada par de adjacentes separados por um espaço. Use as seguintes técnicas:
+//                  a) Uma instrução 'System.out.println';
+//                  b) Quatro instruções 'System.out.print';
+//                  c) uma instrução 'System.out.printf'.
+
+
+public class Ex2p14{
+    public static void main(String[] args){
+        System.out.print("1 2 3 4");
+    }
+}
