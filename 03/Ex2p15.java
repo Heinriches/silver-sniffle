23990deb0e1cde139605341889339be481cf1567
Heinriches <heydrich.institut@gmail.com>
2026-02-05 16:07:29 -0300

    Create Ex2p15.java

---
+++ 03/Ex2p15.java
@@ -0,0 +1,12 @@
+
+//	2.15
+//			(Aritmética) Escreva um aplicativo que solicite ao usuário inserir
+//			dois números-inteiros, obtenha de le estes números e imprima o valor
+//			de sua soma, produto, diferença, e quociente.
+
+
+public class Ex2p15{
+	public static void main(String[] args){
+		
+	}
+}
