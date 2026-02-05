0bd3423e1f24fb29fea75fe4cc74b6a0c3390695
Heinriches <heydrich.institut@gmail.com>
2026-02-05 17:03:36 -0300

    Update Ex2p16.java

--- 04/Ex2p16.java
+++ 04/Ex2p16.java
@@ -16,6 +16,13 @@ public class Ex2p16{
 		
 		int numero1, numero2;
 		
+		System.out.println("Digite o valor do PRIMEIRO número: ");
+		numero1 = sc.nextInt();
+		
+		System.out.println("Digite o valor do SEGUNDO número: ");
+		numero2 = sc.nextInt();
+		
+		
 		if(numero1 == numero2){
 			System.out.println("These numbers are equal");
 		} else if(numero1 > numero2){
