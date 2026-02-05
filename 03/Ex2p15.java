af8350fe42696d02dd3620b2cd3970cd185b7b61
Heinriches <heydrich.institut@gmail.com>
2026-02-05 16:23:18 -0300

    Update Ex2p15.java

--- 03/Ex2p15.java
+++ 03/Ex2p15.java
@@ -1,12 +1,37 @@
 
 //	2.15
-//			(Aritmética) Escreva um aplicativo que solicite ao usuário inserir
-//			dois números-inteiros, obtenha de le estes números e imprima o valor
+//		(Aritmética)
+//			Escreva um aplicativo que solicite ao usuário inserir
+//			dois números-inteiros, obtenha de leia estes números e imprima o valor
 //			de sua soma, produto, diferença, e quociente.
+
+import java.util.Scanner;
 
 
 public class Ex2p15{
+
 	public static void main(String[] args){
 		
+		Scanner sc = new Scanner(System.in);
+		
+		int numero1, numero2, soma, produto, diferenca;
+		double quociente;
+		
+		System.out.println("Digite o valor do PRIMEIRO número: ");
+		numero1 = sc.nextInt();
+		
+		System.out.println("Digite o valor do SEGUNDO número: ");
+		numero2 = sc.nextInt();
+		
+		soma = (numero1 + numero2);
+		produto = (numero1 * numero2);
+		diferenca = (numero1 - numero2);
+		quociente = (numero1 / numero2);
+		
+		System.out.printf("SOMA = %d %n", soma);
+		System.out.printf("PRODUTO = %d %n", produto);
+		System.out.printf("DIFERENÇA = %d %n", diferenca);
+		System.out.printf("QUOCIENTE = %.2f", quociente);
+				
 	}
 }
