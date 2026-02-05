fa46523009f53295fa310c977a8c47b0652ad159
Heinriches <heydrich.institut@gmail.com>
2026-02-05 17:00:36 -0300

    Update Ex2p16.java

--- 04/Ex2p16.java
+++ 04/Ex2p16.java
@@ -12,5 +12,17 @@ import java.util.Scanner;
 public class Ex2.p16{
 	public static void main(String[] args){
 		
+		Scanner sc = new Scanner(System.in);
+		
+		int numero1, numero2;
+		
+		if(numero1 == numero2){
+			System.out.println("These numbers are equal");
+		} else if(numero1 > numero2){
+			System.out.printf("%d is larger %n", numero1);
+		} else{
+			System.out.printf("%d is larger %n", numero2);
+		}
+		
 	}
 }
